10e069e3126320ec867a0c304cd8a9be4af91f2a
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-05-31 19:27:02 +0300

    Update database.py

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     tg_id INTEGER UNIQUE,
+    username TEXT,
     full_name TEXT,
     xj_id TEXT UNIQUE,
     qualification TEXT,
@@ -25,25 +26,19 @@ CREATE TABLE IF NOT EXISTS settings(
 
 cursor.execute("""
 INSERT OR IGNORE INTO settings(id, gift_limit)
-VALUES(1,50)
+VALUES(1, 50)
 """)
 
 db.commit()
 
 
 def check_user(tg_id):
-    cursor.execute(
-        "SELECT * FROM users WHERE tg_id=?",
-        (tg_id,)
-    )
+    cursor.execute("SELECT * FROM users WHERE tg_id=?", (tg_id,))
     return cursor.fetchone()
 
 
 def check_xj_id(xj_id):
-    cursor.execute(
-        "SELECT * FROM users WHERE xj_id=?",
-        (xj_id,)
-    )
+    cursor.execute("SELECT * FROM users WHERE xj_id=?", (xj_id,))
     return cursor.fetchone()
 
 
@@ -53,44 +48,26 @@ def get_count():
 
 
 def get_limit():
-    cursor.execute(
-        "SELECT gift_limit FROM settings WHERE id=1"
-    )
+    cursor.execute("SELECT gift_limit FROM settings WHERE id=1")
     return cursor.fetchone()[0]
 
 
 def set_limit(limit):
-    cursor.execute(
-        "UPDATE settings SET gift_limit=? WHERE id=1",
-        (limit,)
-    )
+    cursor.execute("UPDATE settings SET gift_limit=? WHERE id=1", (limit,))
     db.commit()
 
 
 def add_limit(count):
     current = get_limit()
-
-    cursor.execute(
-        "UPDATE settings SET gift_limit=? WHERE id=1",
-        (current + count,)
-    )
-
+    cursor.execute("UPDATE settings SET gift_limit=? WHERE id=1", (current + count,))
     db.commit()
 
 
-def add_user(
-        tg_id,
-        full_name,
-        xj_id,
-        qualification,
-        phone,
-        address,
-        gift_number
-):
-
+def add_user(tg_id, username, full_name, xj_id, qualification, phone, address, gift_number):
     cursor.execute("""
     INSERT INTO users(
         tg_id,
+        username,
         full_name,
         xj_id,
         qualification,
@@ -98,9 +75,10 @@ def add_user(
         address,
         gift_number
     )
-    VALUES(?,?,?,?,?,?,?)
+    VALUES(?,?,?,?,?,?,?,?)
     """, (
         tg_id,
+        username,
         full_name,
         xj_id,
         qualification,
@@ -108,5 +86,4 @@ def add_user(
         address,
         gift_number
     ))
-
     db.commit()
